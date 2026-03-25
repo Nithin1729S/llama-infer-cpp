01ce1a3ad203fb0c6643943b8b8a499fdf5bd5dd
Nithin1729S <sureshnithin1729@gmail.com>
2026-03-25 17:11:05 +0530

    Remove example matrix multiplication code from main function

--- llama_infer_main.cc
+++ llama_infer_main.cc
@@ -4,17 +4,5 @@
 #include "llama_model.pb.h"
 using namespace llama_infer;
 int main(){
-    std::vector<float> x = {1.f, 2.f, 3.f};
-    std::vector<float> W = {0.5f, 0.5f, 0.5f,
-                            0.5f, 0.5f, 0.5f};
-    std::vector<float> out(2, 0.f);
-
-    matmul(out, x, W, 3, 2);
-
-    std::cout << "Output: ";
-    for (float val : out)
-        std::cout << val << " ";
-    std::cout << std::endl;
-
     return 0;
 }
